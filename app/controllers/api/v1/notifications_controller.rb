97b02d0cc13bbb0fe91a81a312fa7e5cba32fdea
Matthew Kirby <matt.kirby@yahoo.com>
2021-08-08 09:58:13 -0400

    added notification routing plus controller index and show actions

--- app/controllers/api/v1/notifications_controller.rb
+++ app/controllers/api/v1/notifications_controller.rb
@@ -1,2 +1,38 @@
 class Api::V1::NotificationsController < ApplicationController
+  # before the show action runs, calls the private method find_notification,
+  # which sets the @notifications instance variable to the notification record with the params :id
+  before_action :find_notification, only: [:show]
+
+  def index
+    # sets the notification instance variable to an array of all notifications
+    @notifications = Notification.all
+    # instead of rendering a view, renders json data and an accepted status
+    render json: @notifications, status: :accepted
+  end
+
+  def show
+    # runs model validations and returns true of there are no errors
+    if @notification.valid?
+      render json: @notification, status: :ok
+    else
+      # this doesn't seem to ever hit - if the record isn't found by find_notification
+      # it immediately returns active record not found
+      render json: { error: 'notification not found'}, status: :not_found
+    end
+  end
+
+  private
+
+  def find_notification
+    # pulls the param :id out of the request
+    # .find returns the object by :id
+    # if no records exists raises an ActiveRecord::RecordNotFound
+    @notification = Notification.find(params[:id])
+  end
+
+  # need a special route to return just 1 users notifications
+  # /api/v1/users/1
+    # have this return serialized json data with notifications
+  # /api/v1/notifications/?user=:id
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ Rails.application.routes.draw do
   
   namespace :api do
     namespace :v1 do
-      resources :notifications
+      resources :notifications, only: [:index, :show]
       resources :users, only: [:index, :show]
     end
   end
