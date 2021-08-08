c1b4aa1fc2312613b7e2f74935442a214b251157
Matthew Kirby <matt.kirby@yahoo.com>
2021-08-07 21:04:43 -0400

    added user show route

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,8 +1,23 @@
 class Api::V1::UsersController < ApplicationController
+  before_action :find_user, only: [:show]
 
   def index
     @users = User.all
     render json: @users, status: :accepted
   end
-  
+
+  def show
+    if @user.valid?
+      render json: @user, status: :ok
+    else
+      render json: { error: 'user not found'}, status: :not_found
+    end
+  end
+
+  private
+
+  def find_user
+    @user = User.find(params[:id])
+  end
+
 end
