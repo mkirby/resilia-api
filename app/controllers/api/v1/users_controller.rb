d23229114a6af57832244d55ab552b3c6d9caf24
Matthew Kirby <matt.kirby@yahoo.com>
2021-08-08 08:49:51 -0400

    added comments and notes to the users_controller

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,12 +1,17 @@
 class Api::V1::UsersController < ApplicationController
+  # before the show action runs, calls the private method find_user,
+  # which sets the @user instance variable to the user record with the params :id
   before_action :find_user, only: [:show]
 
   def index
+    # sets the users instance variable to an array of all users
     @users = User.all
+    # instead of rendering a view, renders json data and an accepted status
     render json: @users, status: :accepted
   end
 
   def show
+    # runs model validations and returns true of there are no errors
     if @user.valid?
       render json: @user, status: :ok
     else
@@ -17,6 +22,10 @@ class Api::V1::UsersController < ApplicationController
   private
 
   def find_user
+    # pulls the param :id out of the request
+    # .find returns the object by :id
+    # if no records exists raises an ActiveRecord::RecordNotFound
+    # which causes @user.valid? to return false
     @user = User.find(params[:id])
   end
 
