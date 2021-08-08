42f6c4b6bdb1c3d535277817e863d839b32b827b
Matthew Kirby <matt.kirby@yahoo.com>
2021-08-08 11:58:16 -0400

    added name attribute to the user model and serializer

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_08_06_193948) do
+ActiveRecord::Schema.define(version: 2021_08_08_154914) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -27,6 +27,7 @@ ActiveRecord::Schema.define(version: 2021_08_06_193948) do
   create_table "users", force: :cascade do |t|
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "name"
   end
 
   add_foreign_key "notifications", "users"
